6f910e64d0276cee06cfa09e79805cca8589e88f
ufukcalimli <ufk.calimli@gmail.com>
2019-12-11 16:55:48 +0100

    pagination is done

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,30 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import ProductList from '../components/Index/ProductList';
+import ProductPagination from '../components/Index/ProductPagination';
 import baseUrl from '../utils/baseUrl';
 
-function Home({ products }) {
-  return <ProductList products={products} />;
+function Home({ products, totalPages }) {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  );
 }
 
-Home.getInitialProps = async () => {
+Home.getInitialProps = async ctx => {
+  const page = ctx.query.page ? ctx.query.page : '1';
+  const size = 9;
   // Fetch data on server
-  // Return response data as an object
-  // Note: This object will be merged with existing props
-
   const url = `${baseUrl}/api/products`;
-  const response = await axios.get(url);
-  return { products: response.data };
+  const payload = {
+    params: { page, size },
+  };
+  // Return response data as an object
+  const response = await axios.get(url, payload);
+  // Note: This object will be merged with existing props
+  return response.data;
 };
 
 export default Home;
