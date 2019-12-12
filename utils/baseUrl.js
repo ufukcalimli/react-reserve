a19f08a90d2d65bf5f562875ba97128f9b3ab97d
ufukcalimli <ufk.calimli@gmail.com>
2019-12-12 20:15:51 +0100

    baseUrl fixed

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,4 +1,4 @@
 const baseUrl =
-  process.env.NODE_ENV === 'production' ? 'https://ikea.now.sh' : 'http://localhost:3000';
+  process.env.NODE_ENV === 'production' ? 'http://localhost:3000' : 'https://ikea.now.sh';
 
 export default baseUrl;
